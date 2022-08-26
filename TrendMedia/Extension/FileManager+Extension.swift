ba5f833d8a4bc1ae3979c082d655590ee19c3bd5
skyler <ho971122@gmail.com>
2022-08-26 09:58:42 +0900

    쇼핑리스트 Realm, Document 이미지 파일 백업/복구 기능 구현

--- TrendMedia/Extension/FileManager+Extension.swift
+++ TrendMedia/Extension/FileManager+Extension.swift
@@ -12,39 +12,104 @@ extension UIViewController {
     
     //도큐먼트에 이미지 저장
     func saveImageToDocument(fileName: String, image: UIImage?) {
-        guard let document = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        let fileURL = document.appendingPathComponent(fileName)
-        guard let data = image?.jpegData(compressionQuality: 0.5) else { return }
         
-        do {
-            try data.write(to: fileURL)
-        } catch let error {
-            print("file save error", error)
+        let fileManager = FileManager.default
+        
+        guard let document = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return } //Document 경로
+        
+        //이미지를 담을 폴더 생성
+        let directoryPath = document.appendingPathComponent("Images")
+        print("directoryPath: \(directoryPath)")
+        
+        if !fileManager.fileExists(atPath: directoryPath.path) {
+            do {
+                try fileManager.createDirectory(atPath: directoryPath.path, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print("Couldn't create document directory")
+            }
+        }
+        
+        //폴더에 이미지 저장
+        let filePath = directoryPath.appendingPathComponent(fileName)
+        print("filePath: \(filePath)")
+        
+        if !fileManager.fileExists(atPath: filePath.path) {
+            guard let data = image?.jpegData(compressionQuality: 0.5) else { return }
+            do {
+                try data.write(to: filePath)
+            } catch let error {
+                print("이미지 저장 실패", error)
+            }
         }
     }
+    
     
     //도큐먼트에서 이미지 삭제
     func removeImageFromDocument(fileName: String) {
         guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return } //Document 경로
-        let fileURL = documentDirectory.appendingPathComponent(fileName) //세부 경로. 이미지를 저장할 위치
-    
+        let directoryPath = documentDirectory.appendingPathComponent("Images") //폴더 경로
+        let fileURL = directoryPath.appendingPathComponent(fileName) //세부 경로. 이미지를 저장할 위치
+        
         do {
             try FileManager.default.removeItem(at: fileURL)
-        } catch let error {
-            print(error)
+        } catch let error{
+            print("이미지 삭제 실패", error)
         }
     }
     
     //도큐먼트에서 이미지 로드
     func loadImageFromDocument(fileName: String) -> UIImage? {
-        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil } //Document 경로
-        let fileURL = documentDirectory.appendingPathComponent(fileName) //세부 경로. 이미지를 저장할 위치
         
-        if FileManager.default.fileExists(atPath: fileURL.path) {
-            return UIImage(contentsOfFile: fileURL.path)
-        } else {
-            return UIImage(systemName: "heart.fill")
-        }
+        let fileManager = FileManager.default
+        
+        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil } //Document 경로
+        let directoryPath = documentDirectory.appendingPathComponent("Images") //Directory 경로
+        let fileURL = directoryPath.appendingPathComponent(fileName) //세부 경로. 이미지를 저장할 위치
+        print(fileURL)
+//        do {
+            if fileManager.fileExists(atPath: fileURL.path) {
+                return UIImage(contentsOfFile: fileURL.path)
+            } else {
+                return UIImage(systemName: "heart.fill")
+            }
+            
+//        } catch le {
+//            print("이미지 로드 실패")
+//        }
     }
+    
+    //도큐먼트 경로 가져오기
+    func documentDirectoryPath() -> URL? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return documentDirectory
+    }
+    
+    func fetchDocumentZipFile() -> [String] {
+        
+        var result: [String] = []
+        
+        do {
+            guard let path = documentDirectoryPath() else { return [""] }
+            
+            //includingPropertiesForKeys: 더 추가적인 정보를 가져오는 것 [.으로 가져올 수 있음]
+            let docs = try FileManager.default.contentsOfDirectory(at: path, includingPropertiesForKeys: nil)
+            print("docs: \(docs)")
+            
+            //확장자가 zip인 애를 가져오기
+            let zip = docs.filter { $0.pathExtension == "zip" }
+            print("zip: \(zip)")
+            
+            //zip파일 경로에서 가장 마지막 컴포넌트인 zip 파일명만 가져오기
+            result = zip.map { $0.lastPathComponent }
+            print("result: \(result)")
+            
+        } catch {
+            print("Error")
+        }
+        
+        return result
+        
+    }
+    
     
 }
--- TrendMedia/Shopping/ShoppingTableViewController.swift
+++ TrendMedia/Shopping/ShoppingTableViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-import PhotosUI
 
+import PhotosUI
 import RealmSwift
+import Zip
 
 class ShoppingTableViewController: UITableViewController {
     
@@ -25,9 +26,18 @@ class ShoppingTableViewController: UITableViewController {
     
     var objectID: ObjectId?
     
-    let menuButton: UIBarButtonItem = {
+    //정렬 메뉴버튼
+    let menuButton1: UIBarButtonItem = {
         let button = UIBarButtonItem()
         button.image = UIImage(systemName: "list.dash")
+        button.tintColor = .black
+        return button
+    }()
+    
+    //백업/복구 메뉴버튼
+    let menuButton2: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "ellipsis")
         button.tintColor = .black
         return button
     }()
@@ -46,7 +56,9 @@ class ShoppingTableViewController: UITableViewController {
         addButton.setTitle("추가", for: .normal)
         addButton.setTitleColor(.black, for: .normal)
         
-        navigationItem.rightBarButtonItem = menuButton
+        
+        //우측 정렬 메뉴 버튼 추가
+        navigationItem.rightBarButtonItem = menuButton1
         
         let sortContent = UIAction(title: "할 일 기준 정렬") { _ in
             self.tasks = self.localRealm.objects(ShoppingList.self).sorted(byKeyPath: "content", ascending: true)
@@ -60,57 +72,107 @@ class ShoppingTableViewController: UITableViewController {
             self.tasks = self.localRealm.objects(ShoppingList.self).sorted(byKeyPath: "check", ascending: false)
         }
         
-        let cancel = UIAction(title: "취소", attributes: .destructive) { _ in
+        let cancel1 = UIAction(title: "취소", attributes: .destructive) { _ in
             print("취소")
         }
         
-        menuButton.menu = UIMenu(title: "",
-                                 image: UIImage(systemName: "heart.fill"),
-                                 identifier: nil,
-                                 options: .destructive,
-                                 children: [sortContent, sortFavorite, sortCheck, cancel])
+        menuButton1.menu = UIMenu(title: "",
+                                  image: UIImage(systemName: "heart.fill"),
+                                  identifier: nil,
+                                  options: .destructive,
+                                  children: [sortContent, sortFavorite, sortCheck, cancel1])
+        
+        
+        //좌측 백업/복구 버튼 추가
+        navigationItem.leftBarButtonItem = menuButton2
+        
+        let backUp = UIAction(title: "백업") { _ in
+            
+            let fileManager = FileManager.default
+            
+            var urlPaths = [URL]()
+            
+            guard let path = self.documentDirectoryPath() else { return }
+            
+            //데이터베이스 파일
+            let realmFile = path.appendingPathComponent("default.realm")
+            //이미지 파일
+            let imageFile = path.appendingPathComponent("Images")
+            
+            guard fileManager.fileExists(atPath: realmFile.path) else { return }
+            guard fileManager.fileExists(atPath: imageFile.path) else { return }
+            
+            urlPaths.append(URL(string: realmFile.path)!)
+            urlPaths.append(URL(string: imageFile.path)!)
+            
+            //배열 합치기
+//            let combined = Array(zip(urlPaths1, urlPaths2))
+            
+            do {
+                let zipFilePath = try Zip.quickZipFiles(urlPaths , fileName: "List1")
+                print("Archive Location: \(zipFilePath)")
+                self.showActivityViewController()
+            } catch {
+                print("압축 실패")
+            }
+            
+            
+        }
+        
+        let restore = UIAction(title: "복구") { _ in
+            
+            //asCopy를 이용해서 백업 파일을 복사해서 가져올 것인지를 정함
+            let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.archive], asCopy: true)
+            documentPicker.delegate = self
+            documentPicker.allowsMultipleSelection = false
+            self.present(documentPicker, animated: true)
+            
+        }
+        
+        let cancel2 = UIAction(title: "취소", attributes: .destructive) { _ in
+            print("취소")
+        }
+        
+        menuButton2.menu = UIMenu(title: "",
+                                  image: nil,
+                                  identifier: nil,
+                                  options: .displayInline,
+                                  children: [backUp, restore, cancel2])
+        
         
     }
     
-    @IBAction func userTextFieldReturn(_ sender: UITextField) {
+    func showActivityViewController() {
         
-        lazy var task = ShoppingList(content: userTextField.text!)
-        
-        try! localRealm.write {
-            localRealm.add(task)
-            print("저장성공")
-            tableView.reloadData()
+        //도큐먼트 위치 가져오기
+        guard let path = self.documentDirectoryPath() else {
+            print("도큐먼트 위치 오류")
+            return
         }
-        userTextField.text = nil
-        view.endEditing(true)
+        //도큐먼트 내부의 백업할 Realm 파일에 대한 세부 경로를 추가해줌
+        let backUpFileURL = path.appendingPathComponent("List1.zip")
+        
+        let vc = UIActivityViewController(activityItems: [backUpFileURL], applicationActivities: [])
+        self.present(vc, animated: true)
     }
     
     
     @IBAction func addButtonClicked(_ sender: UIButton) {
         
-        let task = ShoppingList(content: userTextField.text!)
-        
-        objectID = task.objectId
-        
-        try! localRealm.write {
-            localRealm.add(task)
-            print("저장성공")
-            tableView.reloadData()
-        }
-        userTextField.text = nil
-        view.endEditing(true)
-        
-        print(localRealm.configuration.fileURL!)
         
         
         //phpicker 생성
-        var configuration = PHPickerConfiguration()
-        configuration.filter = .any(of: [.images])
+        let configuration = PHPickerConfiguration()
         
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
         
         self.present(picker, animated: true)
+        
+        view.endEditing(true)
+        
+        
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -197,21 +259,102 @@ class ShoppingTableViewController: UITableViewController {
 extension ShoppingTableViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         
-        picker.dismiss(animated: true)
+        let task = ShoppingList(content: userTextField.text!)
+        
+        objectID = task.objectId
         
         let itemProvider = results.first?.itemProvider
         
+        try! localRealm.write {
+            localRealm.add(task)
+            print("저장성공")
+            tableView.reloadData()
+        }
+        
         if let itemProvider = itemProvider,
-            itemProvider.canLoadObject(ofClass: UIImage.self) {
+           itemProvider.canLoadObject(ofClass: UIImage.self) {
             itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
                 guard let objectId = self.objectID else { return }
                 guard let itemImage = image else { return }
                 self.saveImageToDocument(fileName: "\(objectId).jpg", image: itemImage as? UIImage)
-                print(objectId)
             }
         }
+        
+        
+        picker.dismiss(animated: true)
     }
 }
+
+extension ShoppingTableViewController: UIDocumentPickerDelegate {
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print(#function)
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        
+        //documentPicker에서 선택한 파일의 URL 가져오기
+        guard let selectedFileURL = urls.first else {
+            print("선택한 파일 없음")
+            return
+        }
+        
+        //도큐먼트 위치 가져오기
+        guard let path = self.documentDirectoryPath() else {
+            print("위치 오류 있음")
+            return
+        }
+        
+        //도큐먼트 위치 내부에서 documentPicker에서 선택한 파일의 URL을 가져오기
+        let sandboxFileURL = path.appendingPathComponent(selectedFileURL.lastPathComponent)
+        
+        //documentPicker에서 선택한 해당 파일이 존재하면 파일 압축 풀기
+        if FileManager.default.fileExists(atPath: sandboxFileURL.path) {
+            
+            print(sandboxFileURL)
+            let fileURL = path.appendingPathComponent("List1.zip")
+            
+            do {
+                try Zip.unzipFile(fileURL, destination: path, overwrite: true, password: nil, progress: { progress in
+                    print("progress: \(progress)")
+                }, fileOutputHandler: { unzippedFile in
+                    print("unzippedFile: \(unzippedFile)")
+                    print("복구 완료")
+                })
+                
+            } catch {
+                print("압축해제 실패1")
+            }
+            
+        } else {
+            
+            //파일이 존재하지 않을 때
+            do {
+                //파일 앱의 zip -> 도큐먼트 폴더에 복사
+                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
+                
+                let fileURL = path.appendingPathComponent("List1.zip") //폴더 생성, 폴더 안에 파일 저장
+                
+                try Zip.unzipFile(fileURL, destination: path, overwrite: true, password: nil, progress: { progress in
+                    print("progress: \(progress)")
+                }, fileOutputHandler: { unzippedFile in
+                    print("unzippedFile: \(unzippedFile)")
+                    print("복구 완료")
+                })
+                
+            } catch {
+                print("압축해제 실패2")
+            }
+            
+        }
+        
+        
+    }
+    
+}
+
+
+
 
 
 
